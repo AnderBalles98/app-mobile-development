efcd6d5fd712ed2d935b637137a7abf1406fa7df
anderballes98 <ander.ballesteros98@gmail.com>
2020-09-30 22:47:38 -0500

    local storage whit appSetings

--- week3/helloworld/src/app/settings/settings.component.ts
+++ week3/helloworld/src/app/settings/settings.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { RadSideDrawer } from "nativescript-ui-sidedrawer";
 import * as app from "tns-core-modules/application";
-import * as dialogs from "tns-core-modules/ui/dialogs";
-import * as Toast from "nativescript-toasts";
+import * as appSetings from "tns-core-modules/application-settings";
 
 @Component({
     selector: "Settings",
@@ -12,43 +11,17 @@ export class SettingsComponent implements OnInit {
 
     constructor() {
         // Use the component constructor to inject providers.
+        appSetings.setBoolean("booleanTest", true);
+        console.log(appSetings.getBoolean("booleanTest", false));
+        appSetings.setString("stringTest", "Messi");
+        console.log(appSetings.getString("stringTest", "Ronaldo"));
+        appSetings.setNumber("numberTest", 89);
+        console.log(appSetings.getNumber("numberTest", 105));
     }
 
-    doLater(fn) {
-        setTimeout(fn, 1000); 
-    }
 
     ngOnInit(): void {
-        // Init your component properties here.
 
-        // interactive windows
-        // this.doLater(() => {
-        //     dialogs.action("Mensaje", "Cancelar!", ["Opcion1", "Opcion2"])
-        //         .then((answear) => {
-        //             if (answear === "Opcion1") {
-        //                 this.doLater(() => {
-        //                     dialogs.alert({
-        //                         title: "Titulo 1",
-        //                         message: "msj 1",
-        //                         okButtonText: "btn 1"
-        //                     }).then(() => console.log("Cerrado 1!"));
-        //                 });
-        //             }
-        //             if (answear === "Opcion2") {
-        //                 this.doLater(() => {
-        //                     dialogs.alert({
-        //                         title: "Titulo 2",
-        //                         message: "msj 2",
-        //                         okButtonText: "btn 2"
-        //                     }).then(() => console.log("Cerrado 2!"));
-        //                 });
-        //             }
-        //         })
-        // });
-        const toastOption: Toast.ToastOptions = {text: "Hola mundo", duration:Toast.DURATION.SHORT};
-        this.doLater(() => {
-            Toast.show(toastOption)
-        });
     }
 
     onDrawerButtonTap(): void {
